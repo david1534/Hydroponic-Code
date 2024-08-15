bb585adf9afbac39be486aa1f157977be049779a
david1534 <danyrice8707@hotmail.com>
2024-08-15 18:33:59 -0400

    Updated: Further Organization

--- Unit Testing/waterSensors/waterSensors.ino
+++ Unit Testing/waterSensors/waterSensors.ino
@@ -1,3 +1,10 @@
+#include <Arduino.h>
+
+//this code tests the water level sensors by itself. This is different from the simulation
+//code because LOW and HIGH values are swapped. When the actual water level sensors detect water
+//the signal recieved will be 0V or LOW. In the simulation, it is implemented the opposite 
+//way (5v or HIGH) because of the way simulIDE works. 
+
 const int SensorA = 5;
 const int SensorB = 6;
 const int SensorC = 7;
