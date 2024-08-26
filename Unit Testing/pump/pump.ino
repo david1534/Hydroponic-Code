832128a105af34c1b2d32194a1274e4395306c02
anthonyw298 <163664425+anthonyw298@users.noreply.github.com>
2024-08-26 19:11:05 -0400

    Added Code for Pump.ino Unit testing utilizing the same structure code as fans.ino unit testing code
    THis code will initialize the pump to move with the testing of the pump speed and being able to adjust it

--- Unit Testing/pump/pump.ino
+++ Unit Testing/pump/pump.ino
@@ -1,1 +1,14 @@
 #include <Arduino.h>
+
+const int pumpPin = 11; 
+int pumpPWM = 0; // PWM value for controlling pump speed
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(pumpPin, OUTPUT); // Set the pump pin as an output
+}
+
+void loop() {
+    //analogWrite(pumpPin, pumpPWM); // Uncomment to control pump with PWM
+    digitalWrite(pumpPin, HIGH); // Turn the pump on
+}
